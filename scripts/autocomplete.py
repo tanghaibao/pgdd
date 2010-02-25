1104723fe5afdd730681f806cfa44c26e81f53a9
Haibao Tang <tanghaibao@gmail.com>
2010-02-25 16:19:06 -0500

    dotplot web page species name fill in

--- scripts/autocomplete.py
+++ scripts/autocomplete.py
@@ -4,7 +4,7 @@ from preferences import myconnect
 
 def index(req):
     q = req.form.getfirst("q")
-    query = "SELECT locus FROM loci WHERE locus LIKE '%s%%' ORDER by locus limit 5"%q
+    query = "SELECT locus FROM loci WHERE locus LIKE '%s%%' ORDER by locus limit 5" % q
     res = myconnect(query)
-    out = "\n".join([x[0] for x in res])
+    out = "\n".join(x[0] for x in res)
     return out

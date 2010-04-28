06dc9b46c55db17df040cd0c36909e5df4595028
Haibao Tang <tanghaibao@gmail.com>
2010-04-27 21:44:11 -0400

    server script for running treecut

--- cut/scripts/cut.py
+++ cut/scripts/cut.py
@@ -1,2 +1,44 @@
+import os
+import os.path as op
+from random import randint
+
+
+# Generator to buffer file chunks
+def fbuffer(f, chunk_size=10000):
+   while True:
+      chunk = f.read(chunk_size)
+      if not chunk: break
+      yield chunk
+
+
+def write_to_file(str):
+    fname = "tmp_%05d" % randint(0, 100000)
+    fname = "/var/www/duplication/usr/" + fname
+    if str:
+        f = open(fname, "wb", 10000)
+        f.write(str)
+    return fname
+
+
 def index(req):
-    return "Hello world!"
+
+    tree_str = req.form['tree']
+    list_str = req.form['list']
+
+    if tree_str:
+        tree_f = write_to_file(tree_str) 
+    if list_str:
+        list_f = write_to_file(list_str)
+
+    out_f = write_to_file(None) + ".svg"
+    cmd = "/usr/bin/python /var/www/duplication/apps/treecut/treecut.py %s %s %s" % (tree_f, list_f, out_f)
+    stdin, stdout, stderr = os.popen3(cmd)
+    stdout_str, stderr_str = stdout.read(), stderr.read()
+    #return "Cmd:%s\nOut:\n%s\nError:\n%s" % (cmd, stdout_str, stderr_str) 
+
+    # cleanup
+    for f in (tree_f, list_f): 
+        if op.exists(f): os.remove(f)
+
+    #return "<object data='%s' width='640' height='640' type='image/svg+xml' codebase='http://www.adobe.com/svg/viewer/install/' />" % (out_f.replace("/var/www", ""))
+    return "<iframe src='%s' width='640' height='640'></iframe>" % (out_f.replace("/var/www", ""))
